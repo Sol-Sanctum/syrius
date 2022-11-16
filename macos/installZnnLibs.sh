6bb1803fd5a81fccd2f8ef8bb803e0d5a1034196
Sol Sanctum <so1_sanctum@pm.me>
2022-11-15 23:47:33 -0500

    Added build support for MacOS x86_64

--- macos/installZnnLibs.sh
+++ macos/installZnnLibs.sh
@@ -1,13 +1,23 @@
 #!/bin/bash
 # installZnnLibs.sh
-# Executed at the end of Podfile in order to copy Zenon libraries to the build target directory.
+# Executed at the end of the build process in order to copy Zenon libraries to the build output directory.
 
-echo "------------------"
-echo $(TARGET_BUILD_DIR)/$(UNLOCALIZED_RESOURCES_FOLDER_PATH)/
-echo "------------------"
+OUTPUT_DIRECTORY="$CONFIGURATION_BUILD_DIR/$UNLOCALIZED_RESOURCES_FOLDER_PATH/"
+SYRIUS_PROJECT_DIRECTORY="$SOURCE_ROOT/.."
 
-PACKAGE_CONFIG=../.dart_tool/package_config.json
+PACKAGE_CONFIG="../.dart_tool/package_config.json"
+ZNN_SDK_DART_PATH=`cat $PACKAGE_CONFIG | grep git/znn_sdk_dart | sed 's/"rootUri": "file:\/\///' | sed 's/\/",//' | xargs`  
 
-while read line; do
-        echo $line
-done < PACKAGE_CONFIG
+SYRIUS_LIBRARIES=(
+  "$SYRIUS_PROJECT_DIRECTORY/lib/embedded_node/blobs/libznn.dylib"
+  "$SYRIUS_PROJECT_DIRECTORY/lib/swap/libExportWallet.dylib"
+  "$ZNN_SDK_DART_PATH/lib/src/argon2/blobs/libargon2_ffi.dylib"
+  "$ZNN_SDK_DART_PATH/lib/src/pow/blobs/libpow_links.dylib"
+)
+
+echo "$OUTPUT_DIRECTORY"
+for znn_library in ${SYRIUS_LIBRARIES[@]}; do
+  echo "Install znn library: $znn_library"
+  cp $znn_library "$OUTPUT_DIRECTORY"
+done
+
